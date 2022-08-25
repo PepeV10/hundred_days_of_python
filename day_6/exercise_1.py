79bcf2b635ab849f1f6effae61288f752c25a60e
Pepe <jo.ve.or.84@gmail.com>
2022-08-24 21:51:46 -0500

    Day 6: Exercise 1 - Reeborg's World Hurdles Challenge

---
+++ day_6/exercise_1.py
@@ -0,0 +1,16 @@
+#Reeborg's World - Hurdles Challenge
+def turn_right():
+  turn_left()
+  turn_left()
+  turn_left()
+def jump():
+  move()
+  turn_left()
+  move()
+  turn_right()
+  move()
+  turn_right()
+  move()
+  turn_left()
+for step in range(6):
+  jump()
