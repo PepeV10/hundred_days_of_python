73de36fbbe1fe8041df7fa6150b85d2c8608aeb5
Pepe <jo.ve.or.84@gmail.com>
2022-08-27 18:40:16 -0500

    Day 6: Exercise 4 - Hurdles Challenge Using While Loops

---
+++ day_6/exercise_4.py
@@ -0,0 +1,22 @@
+# Hurdles Challenge Using While Loops
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+    
